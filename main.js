08f074141c80548eac8292c56aec3064b3dd003f
Jennifer <87148355+JenniferSieber@users.noreply.github.com>
2023-11-03 03:09:39 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 // www.codewars.com
 // KATA 1 8kyu
 const positiveSum = arr => {
-  let total = 0
+  let total = 0;
   const test = arr.forEach(num => {
     if (num > 0) {
-      total+= num
+      total+= num;
     }
   })
-  return total
+  return total;
 }
 
 console.log(positiveSum([1,-4,7,12]));
 
 // KATA 2 6kyu
 const solution = num => {
-  let sum = 0
+  let sum = 0;
  for (let i = 1; i < num; i++) {
  if (i % 3 === 0 || i % 5 === 0) {
-    sum += i
+    sum += i;
   } 
  } 
- return sum
+ return sum;
 }
 
 console.log(solution(15));
